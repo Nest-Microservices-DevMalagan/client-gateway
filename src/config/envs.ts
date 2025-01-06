97f67c32d57f6867f9deaa0c44bf65236476c48a
AgnerVillaFabrega <adejesusvilla@unicesar.edu.co>
2025-01-06 00:44:52 -0500

    Section-06
    Order microservice

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,17 +1,31 @@
 import 'dotenv/config';
 import * as joi from 'joi';
-import { string } from 'joi';
 
 interface EnvVars{
+    //? Puerto del gateway
     PORT:number;    
+
+    //! Producs microservices
     PRODUCTS_MICROSERVICES_HOST:string;
     PRODUCTS_MICROSERVICES_PORT:number;
+    
+    //! Orders microservices
+    ORDERS_MICROSERVICES_HOST:string;
+    ORDERS_MICROSERVICES_PORT:number;
+
 }
 
 const envsSchema = joi.object({
+    //? Puerto del gateway
     PORT:joi.number().required(),
+
+    //! Producs microservices
     PRODUCTS_MICROSERVICES_HOST: joi.string().required(),
     PRODUCTS_MICROSERVICES_PORT: joi.number().required(),
+
+    //! Orders microservices
+    ORDERS_MICROSERVICES_HOST: joi.string().required(),
+    ORDERS_MICROSERVICES_PORT: joi.number().required(),
 })
 .unknown()
 
@@ -23,7 +37,14 @@ const envVars: EnvVars = value;
 
 
 export const envs = {
+     //? Puerto del gateway
     port: envVars.PORT,
+
+    //! Producs microservices
     producsMicroserviceHost:envVars.PRODUCTS_MICROSERVICES_HOST,
     producsMicroservicePort:envVars.PRODUCTS_MICROSERVICES_PORT,
+
+    //! Orders microservices
+    ordersMicroserviceHost:envVars.ORDERS_MICROSERVICES_HOST,
+    ordersMicroservicePort:envVars.ORDERS_MICROSERVICES_PORT,
 };
